c56af339197a18b918563f3258f60830f9ad4d24
zackthomas1 <zackthomas13@gmail.com>
2025-12-18 12:23:56 -0800

    Complete 2D camera tutorial.

--- src/main.c
+++ src/main.c
@@ -10,7 +10,7 @@ by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit h
 #include "raylib.h"
 #include "resource_dir.h"	// utility header for SearchAndSetResourceDir
 
-#define MAX_TOUCH_POINTS 10
+#define MAX_BUILDINGS 100
 
 // ------------------------
 // Program main entry point
@@ -33,23 +33,81 @@ int main ()
     int currentFPS = 60;
     SetTargetFPS(currentFPS); 
 
-    Vector2 touchPositions[MAX_TOUCH_POINTS] = { 0 };
+    Rectangle player = { 400, 280, 40, 40 };
+    Rectangle buildings[MAX_BUILDINGS] = { 0 };
+    Color buildColors[MAX_BUILDINGS] = { 0 };
+
+    int spacing = 0;
     
+    for (int i = 0; i < MAX_BUILDINGS; i++) {
+        buildings[i].width = (float)GetRandomValue(50, 200);
+        buildings[i].height = (float)GetRandomValue(100, 800);
+        buildings[i].y = screenHeight - 130.0f - buildings[i].height;
+        buildings[i].x = -6000.00f + spacing;
+
+        spacing += (int)buildings[i].width;
+
+        buildColors[i] = (Color){
+            (unsigned char)GetRandomValue(200, 240),
+            (unsigned char)GetRandomValue(200, 240),
+            (unsigned char)GetRandomValue(200, 250),
+            255
+        };
+    }
+
+    Camera2D camera = { 0 }; 
+    camera.target = (Vector2){ player.x + 20.0f, player.y + 20.0f };
+    camera.offset = (Vector2){ screenWidth / 2.0f, screenHeight / 2.0f };
+    camera.rotation = 0.0f; 
+    camera.zoom = 1.0f;
+
     // Main game loop
     while (!WindowShouldClose())        // run the loop until the user presses ESCAPE or presses the Close button on the window
     {
         // Update
         // -----------------------
-        // Get the touch point count (how many fingers are touching screen)
-        int tCount = GetTouchPointCount();
-
-        // Clamp touch points available
-        if (tCount > MAX_TOUCH_POINTS) {
-            tCount = MAX_TOUCH_POINTS;
+        // player movement
+        if (IsKeyDown(KEY_RIGHT)) { 
+            player.x += 2; 
+        }
+        else if (IsKeyDown(KEY_LEFT)) {
+            player.x -= 2;
         }
 
-        for (int i = 0; i < tCount; i++) {
-            touchPositions[i] = GetTouchPosition(i);
+        // camera target follows player
+        camera.target = (Vector2){ player.x + 20, player.y + 20 };
+
+        // Camera rotation controls
+        if (IsKeyDown(KEY_A)) {
+            camera.rotation--;
+        }
+        else if (IsKeyDown(KEY_S)) {
+            camera.rotation++;
+        }
+
+        // limit camera rotation to 80 degrees [-40,40]
+        if (camera.rotation > 40) {
+            camera.rotation = 40;
+        }
+        else if (camera.rotation < -40) {
+            camera.rotation = -40;
+        }
+
+        // Camera zoom controls
+        // Use log scaling to provide consistent zoom speed
+        camera.zoom = camera.zoom + ((float)GetMouseWheelMove() * 0.1f);
+
+        if (camera.zoom > 3.0f) {
+            camera.zoom = 3.0f;
+        }
+        else if (camera.zoom < 0.1f) {
+            camera.zoom = 0.1f;
+        }
+
+        // camera reset (zoom/rotation)
+        if (IsKeyPressed(KEY_R)) {
+            camera.zoom = 1.0f;
+            camera.rotation = 0.0f;
         }
 
         // -----------------------
@@ -57,19 +115,42 @@ int main ()
         // Drawing
         // ------------------------
         BeginDrawing();
+        {
+            // Setup the back buffer for drawing (clear color and depth buffers)
+            ClearBackground(RAYWHITE);
 
-        // Setup the back buffer for drawing (clear color and depth buffers)
-        ClearBackground(RAYWHITE);
+            BeginMode2D(camera);
+            {
+                DrawRectangle(-6000, 320, 13000, 8000, DARKGRAY);
 
-        for (int i = 0; i < tCount; i++) {
-            if ((touchPositions[i].x > 0) && (touchPositions[i].y > 0)) {
-                // Draw circle and touch index number 
-                DrawCircleV(touchPositions[i], 34, ORANGE); 
-                DrawText(TextFormat("%d", i), (int)touchPositions[i].x - 10, (int)touchPositions[i].y - 70, 40, BLACK);
+                for (int i = 0; i < MAX_BUILDINGS; i++) {
+                    DrawRectangleRec(buildings[i], buildColors[i]);
+                }
+
+                DrawRectangleRec(player, RED);
+
+                DrawLine((int)camera.target.x, -screenHeight * 10, (int)camera.target.x, screenHeight * 10, GREEN);
+                DrawLine(-screenWidth * 10, (int)camera.target.y, screenWidth * 10, (int)camera.target.y, GREEN);
             }
-        }
+            EndMode2D();
 
-        DrawText("touch the screen at multiple locations to get multiple balls", 10, 10, 20, DARKGRAY);
+            DrawText("SCREEN AREA", 640, 10, 20, RED);
+
+
+            DrawRectangle(0, 0, screenWidth, 5, RED);
+            DrawRectangle(0, 5, 5, screenHeight - 10, RED);
+            DrawRectangle(screenWidth - 5, 5, 5, screenHeight - 10, RED);
+            DrawRectangle(0, screenHeight - 5, screenWidth, 5, RED);
+
+            DrawRectangle(10, 10, 250, 113, Fade(SKYBLUE, 0.5f));
+            DrawRectangleLines(10, 10, 250, 113, BLUE);
+
+            DrawText("Free 2D camera controls:", 20, 20, 10, BLACK);
+            DrawText("- Right/Left to move player", 40, 40, 10, DARKGRAY);
+            DrawText("- Mouse Wheel to Zoom in-out", 40, 60, 10, DARKGRAY);
+            DrawText("- A / S to Rotate", 40, 80, 10, DARKGRAY);
+            DrawText("- R to reset Zoom and Rotation", 40, 100, 10, DARKGRAY);
+        }
 
         // end the frame and get ready for the next one  (display frame, poll input, etc...)
         EndDrawing();
