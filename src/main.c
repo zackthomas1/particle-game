45e4f07719c94d7c719e1418db5e633f367bb2c8
zackthomas1 <zackthomas13@gmail.com>
2025-12-18 14:32:43 -0800

    2D camera mouse move tutorial complete

--- src/main.c
+++ src/main.c
@@ -8,6 +8,9 @@ by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit h
 */
 
 #include "raylib.h"
+#include "rlgl.h"
+#include "raymath.h"
+
 #include "resource_dir.h"	// utility header for SearchAndSetResourceDir
 
 #define MAX_BUILDINGS 100
@@ -33,84 +36,75 @@ int main ()
     int currentFPS = 60;
     SetTargetFPS(currentFPS); 
 
-    Rectangle player = { 400, 280, 40, 40 };
-    Rectangle buildings[MAX_BUILDINGS] = { 0 };
-    Color buildColors[MAX_BUILDINGS] = { 0 };
-
-    int spacing = 0;
-    
-    for (int i = 0; i < MAX_BUILDINGS; i++) {
-        buildings[i].width = (float)GetRandomValue(50, 200);
-        buildings[i].height = (float)GetRandomValue(100, 800);
-        buildings[i].y = screenHeight - 130.0f - buildings[i].height;
-        buildings[i].x = -6000.00f + spacing;
-
-        spacing += (int)buildings[i].width;
-
-        buildColors[i] = (Color){
-            (unsigned char)GetRandomValue(200, 240),
-            (unsigned char)GetRandomValue(200, 240),
-            (unsigned char)GetRandomValue(200, 250),
-            255
-        };
-    }
-
     Camera2D camera = { 0 }; 
-    camera.target = (Vector2){ player.x + 20.0f, player.y + 20.0f };
-    camera.offset = (Vector2){ screenWidth / 2.0f, screenHeight / 2.0f };
-    camera.rotation = 0.0f; 
     camera.zoom = 1.0f;
+
+    int zoomMode = 0;   // 0 - Mouse Wheel, 1 - Mouse Move
 
     // Main game loop
     while (!WindowShouldClose())        // run the loop until the user presses ESCAPE or presses the Close button on the window
     {
         // Update
         // -----------------------
-        // player movement
-        if (IsKeyDown(KEY_RIGHT)) { 
-            player.x += 2; 
+        if (IsKeyPressed(KEY_ONE)) {
+            zoomMode = 0; 
         }
-        else if (IsKeyDown(KEY_LEFT)) {
-            player.x -= 2;
-        }
-
-        // camera target follows player
-        camera.target = (Vector2){ player.x + 20, player.y + 20 };
-
-        // Camera rotation controls
-        if (IsKeyDown(KEY_A)) {
-            camera.rotation--;
-        }
-        else if (IsKeyDown(KEY_S)) {
-            camera.rotation++;
+        else if (IsKeyPressed(KEY_TWO)) {
+            zoomMode = 1;
         }
 
-        // limit camera rotation to 80 degrees [-40,40]
-        if (camera.rotation > 40) {
-            camera.rotation = 40;
-        }
-        else if (camera.rotation < -40) {
-            camera.rotation = -40;
-        }
-
-        // Camera zoom controls
-        // Use log scaling to provide consistent zoom speed
-        camera.zoom = camera.zoom + ((float)GetMouseWheelMove() * 0.1f);
-
-        if (camera.zoom > 3.0f) {
-            camera.zoom = 3.0f;
-        }
-        else if (camera.zoom < 0.1f) {
-            camera.zoom = 0.1f;
+        // Translate based on mouse left click
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+            Vector2 delta = GetMouseDelta(); 
+            delta = Vector2Scale(delta, -1.0f / camera.zoom); 
+            camera.target = Vector2Add(camera.target, delta);
         }
 
-        // camera reset (zoom/rotation)
-        if (IsKeyPressed(KEY_R)) {
-            camera.zoom = 1.0f;
-            camera.rotation = 0.0f;
-        }
+        if (zoomMode == 0) {
+            // Zoom based on mouse wheel
+            float wheel = GetMouseWheelMove(); 
+            if (wheel != 0) {
 
-        // -----------------------
+                // Get world point that is under mouse
+                Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), camera);
+
+                // Set offset to where mouse is
+                camera.offset = GetMousePosition(); 
+
+                // Set targeet to match, so that camera maps world space point
+                // under cursor to screen space point under cursor at any zoom
+                camera.target = mouseWorldPos; 
+
+                // zoom increment
+                // Use log scalling
+                float scale = 0.2f * wheel; 
+                camera.zoom = Clamp(expf(logf(camera.zoom) + scale), 0.125f, 64.0f);
+            }
+        }
+        else {
+            // Zoom based on mouse right click
+            if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT))
+            {
+                // Get the world point that is under the mouse
+                Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), camera);
+
+                // Set the offset to where the mouse is
+                camera.offset = GetMousePosition();
+
+                // Set the target to match, so that the camera maps the world space point
+                // under the cursor to the screen space point under the cursor at any zoom
+                camera.target = mouseWorldPos;
+            }
+
+            if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT))
+            {
+                // Zoom increment
+                // Uses log scaling to provide consistent zoom speed
+                float deltaX = GetMouseDelta().x;
+                float scale = 0.005f * deltaX;
+                camera.zoom = Clamp(expf(logf(camera.zoom) + scale), 0.125f, 64.0f);
+            }
+        }
 
         // Drawing
         // ------------------------
@@ -121,40 +115,35 @@ int main ()
 
             BeginMode2D(camera);
             {
-                DrawRectangle(-6000, 320, 13000, 8000, DARKGRAY);
-
-                for (int i = 0; i < MAX_BUILDINGS; i++) {
-                    DrawRectangleRec(buildings[i], buildColors[i]);
+                // draw the 3D grid, rotated 90 degrees and centered around (0,0)
+                rlPushMatrix();
+                {
+                    rlTranslatef(0, 25*50, 0);
+                    rlRotatef(90, 1, 0, 0);
+                    DrawGrid(100, 50);
                 }
+                rlPopMatrix();
 
-                DrawRectangleRec(player, RED);
-
-                DrawLine((int)camera.target.x, -screenHeight * 10, (int)camera.target.x, screenHeight * 10, GREEN);
-                DrawLine(-screenWidth * 10, (int)camera.target.y, screenWidth * 10, (int)camera.target.y, GREEN);
+                // Draw referencee circle
+                DrawCircle(GetScreenWidth() / 2, GetScreenHeight() / 2, 50, MAROON);
             }
             EndMode2D();
 
-            DrawText("SCREEN AREA", 640, 10, 20, RED);
+            // Draw mouse reference
+            // Vector2 mousePos = GetWorldToScreen2D(GetMousePosition(), camera)
+            DrawCircleV(GetMousePosition(), 4, DARKGRAY);
+            DrawTextEx(GetFontDefault(), TextFormat("[%i, %i]", GetMouseX(), GetMouseY()),
+                Vector2Add(GetMousePosition(), (Vector2) { -44, -24 }), 20, 2, BLACK);
 
-
-            DrawRectangle(0, 0, screenWidth, 5, RED);
-            DrawRectangle(0, 5, 5, screenHeight - 10, RED);
-            DrawRectangle(screenWidth - 5, 5, 5, screenHeight - 10, RED);
-            DrawRectangle(0, screenHeight - 5, screenWidth, 5, RED);
-
-            DrawRectangle(10, 10, 250, 113, Fade(SKYBLUE, 0.5f));
-            DrawRectangleLines(10, 10, 250, 113, BLUE);
-
-            DrawText("Free 2D camera controls:", 20, 20, 10, BLACK);
-            DrawText("- Right/Left to move player", 40, 40, 10, DARKGRAY);
-            DrawText("- Mouse Wheel to Zoom in-out", 40, 60, 10, DARKGRAY);
-            DrawText("- A / S to Rotate", 40, 80, 10, DARKGRAY);
-            DrawText("- R to reset Zoom and Rotation", 40, 100, 10, DARKGRAY);
+            DrawText("[1][2] Select mouse zoom mode (Wheel or Move)", 20, 20, 20, DARKGRAY);
+            if (zoomMode == 0) {
+                DrawText("Mouse left button drag to move, mouse wheel to zoom", 20, 50, 20, DARKGRAY);
+            } else {
+                DrawText("Mouse left button drag to move, mouse press and move to zoom", 20, 50, 20, DARKGRAY);
+            }
         }
-
         // end the frame and get ready for the next one  (display frame, poll input, etc...)
         EndDrawing();
-        // ------------------------
     }
     // De-Initialization
     // ------------------------
