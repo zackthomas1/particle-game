cab4b16680476a3e265928de6e0cfe13f975a0b1
zackthomas1 <zackthomas13@gmail.com>
2025-12-18 11:19:41 -0800

    complete delta time tutorial

--- src/main.c
+++ src/main.c
@@ -22,7 +22,18 @@ int main ()
     SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
     // Create the window and OpenGL context
-    InitWindow(1280, 800, "Hello Raylib");
+    const int screenWidth = 800; 
+    const int screenHeight = 450;
+    InitWindow(screenWidth, screenHeight, "Hello Raylib");
+
+    int currentFPS = 60;
+    SetTargetFPS(currentFPS); 
+
+    // Sotre the position for both circles
+    Vector2 deltaCircle = { 0, (float)screenHeight / 3.0f };
+    Vector2 frameCircle = { 0, (float)screenHeight * (2.0f / 3.0f) };
+
+    const float speed = 10.0f, circleRadius = 32.0f;
 
     // Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
     SearchAndSetResourceDir("resources");
@@ -33,19 +44,73 @@ int main ()
     // Main game loop
     while (!WindowShouldClose())        // run the loop until the user presses ESCAPE or presses the Close button on the window
     {
+        // Update
+        // -----------------------
+        // Adjust FPS target based on mouse wheel
+        float mouseWheel = GetMouseWheelMove();
+        if (mouseWheel != 0) {
+            currentFPS += (int)mouseWheel;
+            if (currentFPS < 0) {
+                currentFPS = 0;
+            }
+            SetTargetFPS(currentFPS);
+        }
+
+        // GetFrameTime() returns the time it took to draw last frame, in seconds(delta time)
+        // Use delta time to make cicle look like it's moving at a consistent speed reargless of actual FPS
+
+        // Multiple by arbitrary value to make speed
+        // visually closer to other cirlce (at 60 fps), for comparison 
+        const float deltaMultiplier = 6.0f;
+        deltaCircle.x += GetFrameTime() * deltaMultiplier * speed;
+
+        // This circle can move faster or slower depending on FPS
+        frameCircle.x += 0.1f * speed;
+
+        if (deltaCircle.x > screenWidth) {
+            deltaCircle.x = 0;
+        }
+
+        if (frameCircle.x > screenWidth) {
+            frameCircle.x = 0;
+        }
+
+        // Reset both circles positions
+        if (IsKeyPressed(KEY_R)) {
+            deltaCircle.x = 0; 
+            frameCircle.x = 0;
+        }
+
+        // -----------------------
+
         // Drawing
         // ------------------------
         BeginDrawing();
 
         // Setup the back buffer for drawing (clear color and depth buffers)
-        ClearBackground(BLACK);
+        ClearBackground(RAYWHITE);
 
-        // draw some text using the default font
-        DrawText("Hello Raylib", 200,200,20,WHITE);
+        // Draw both circles to screen
+        DrawCircleV(deltaCircle, circleRadius, RED);
+        DrawCircleV(frameCircle, circleRadius, BLUE);
 
-        // draw our texture to the screen
-        DrawTexture(wabbit, 400, 200, WHITE);
-        
+        // draw help text
+        const char* fpsText = 0; 
+        if (currentFPS <= 0) {
+            fpsText = TextFormat("FPS: unlimited (%i)", GetFPS());
+        }
+        else {
+            TextFormat("FPS: %i (target: %i)", GetFPS(), currentFPS);
+        }
+
+        DrawText(fpsText, 10, 10, 20, DARKGRAY);
+        DrawText(TextFormat("Frame time: %02.02f ms", GetFrameTime()), 10, 30, 20, DARKGRAY);
+        DrawText("Use the scroll wheel to change fps limit, r to reset", 10, 50, 20, DARKGRAY);
+
+        // draw text above circles 
+        DrawText("Func: x += GetFrameTime()*speed", 10, 90, 20, RED);
+        DrawText("Func: x += speed", 10, 240, 20, BLUE);
+
         // end the frame and get ready for the next one  (display frame, poll input, etc...)
         EndDrawing();
 
