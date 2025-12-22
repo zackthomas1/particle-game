3f2298fb544a454e3acde2659247e34a25a90961
zackthomas1 <zackthomas13@gmail.com>
2025-12-22 09:36:18 -0800

    Refactor UpdateParticles method to remove need for pIsDead array.

--- src/particle.c
+++ src/particle.c
@@ -10,7 +10,7 @@
 // }
 
 static const Vector2 INITIAL_VELOCITY   = { 0.0f, -10.0f};
-static const float INITIAL_LIFETIME     = 10.0f;
+static const float INITIAL_LIFETIME     = 5.0f;
 static const Color PARTICLE_COLOR       = RED;
 static const float PARTICLE_RADIUS      = 4.0f;
 
@@ -28,7 +28,6 @@ ParticleFactory* ConstructParticleFactory()
         factory->pAccelations[i]    = (Vector2){ 0 };
 
         factory->pLifetimes[i]  = 0.0f;
-        factory->pIsDead[i]     = false;
     }
 
     return factory;
@@ -45,7 +44,6 @@ void SpawnParticle(ParticleFactory* factory)
     size_t i = factory->activeCount;
 
     factory->pLifetimes[i]      = INITIAL_LIFETIME;
-    factory->pIsDead[i]         = false;
 
     factory->pPositions[i]      = factory->position;
     factory->pVelocities[i]     = INITIAL_VELOCITY;
@@ -54,46 +52,45 @@ void SpawnParticle(ParticleFactory* factory)
     factory->activeCount += 1;
 }
 
-void KillParticle(ParticleFactory* factory, int index) 
+void KillParticle(ParticleFactory* factory, size_t index) 
 {
     factory->activeCount--;
-
-    factory->pLifetimes[index]      = factory->pLifetimes[factory->activeCount];
-    factory->pIsDead[index]         = factory->pIsDead[factory->activeCount];
-
-    factory->pPositions[index]      = factory->pPositions[factory->activeCount];
-    factory->pVelocities[index]     = factory->pVelocities[factory->activeCount];
-    factory->pAccelations[index]    = factory->pAccelations[factory->activeCount];
+    SwapParticles_(factory, index, factory->activeCount);
 }
 
 void UpdateParticles(ParticleFactory* factory, float deltaTime) 
 {
-    uint32_t deadCount = 0;
+    size_t deadCount = 0;
     for (size_t i = 0; i < factory->activeCount; i++) 
     {
         factory->pLifetimes[i]  -= deltaTime;
         if (factory->pLifetimes[i] < 0.0f) 
         { 
-            factory->pIsDead[i] = true; 
-            continue; 
+            deadCount++;
+            SwapParticles_(factory, i, (factory->activeCount - deadCount));
         }
     }
+    factory->activeCount -= deadCount;
 
     for (size_t i = 0; i < factory->activeCount; i++)
     {
         factory->pVelocities[i]  = Vector2Add(factory->pVelocities[i], Vector2Scale(factory->pAccelations[i], deltaTime));
         factory->pPositions[i]   = Vector2Add(factory->pPositions[i], Vector2Scale(factory->pVelocities[i], deltaTime));
     }
-
-    for(size_t i = 0; i < factory->activeCount; i++)
-    {
-        if (factory->pIsDead[i]) { KillParticle(factory, i); }
-    }
 }
 
 void DrawParticles(ParticleFactory* factory) 
 {
-    for (int i = 0; i < factory->activeCount; i++){
+    for (size_t i = 0; i < factory->activeCount; i++){
         DrawCircleV(factory->pPositions[i], PARTICLE_RADIUS, PARTICLE_COLOR);
     }
+}
+
+static void SwapParticles_(ParticleFactory* factory, size_t i, size_t j)
+{
+    factory->pLifetimes[i]      = factory->pLifetimes[j];
+
+    factory->pPositions[i]      = factory->pPositions[j];
+    factory->pVelocities[i]     = factory->pVelocities[j];
+    factory->pAccelations[i]    = factory->pAccelations[j];
 }
--- src/particle.h
+++ src/particle.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "raylib.h"
 
-#define MAX_PARTICLE_COUNT 100
+#define MAX_PARTICLE_COUNT 1000
 
 typedef enum { 
     WATER, 
@@ -11,10 +11,9 @@ typedef enum {
 typedef struct ParticleFactory 
 {
     Vector2 position;
-    uint32_t activeCount;
+    size_t activeCount;
 
     float pLifetimes[MAX_PARTICLE_COUNT];
-    bool pIsDead[MAX_PARTICLE_COUNT];
 
     Vector2 pPositions[MAX_PARTICLE_COUNT];
     Vector2 pVelocities[MAX_PARTICLE_COUNT];
@@ -33,6 +32,8 @@ ParticleFactory* ConstructParticleFactory();
 void DestructParticleFactory(ParticleFactory* factory);
 
 void SpawnParticle(ParticleFactory* factory);
-void KillParticle(ParticleFactory* factory, int index);
+void KillParticle(ParticleFactory* factory, size_t index);
 void UpdateParticles(ParticleFactory* factory, float deltaTime);
 void DrawParticles(ParticleFactory* factory);
+
+static void SwapParticles_(ParticleFactory* factory, size_t i, size_t j);
