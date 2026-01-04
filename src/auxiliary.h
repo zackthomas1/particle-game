ec2bc057f3a734d2c6339d5b628dd17d6c7af23b
zackthomas1 <zackthomas13@gmail.com>
2026-01-04 00:28:10 -0800

    Remove custom reflect vector function use raylib implementation instead.

--- src/auxiliary.h
+++ src/auxiliary.h
@@ -28,12 +28,3 @@ inline static float GetRandomValueF()
 {
     return ((2.0f * ((float)GetRandomValue(0, INT32_MAX) / (float)INT32_MAX)) - 1.0f);
 }
-
-inline static Vector2 ReflectV(Vector2 vector, Vector2 surfaceNormal)
-{
-    // R = V - 2 * (V ⋅ N) * N
-    return Vector2Subtract(vector, 
-            Vector2Scale(surfaceNormal,
-                (2.0f * Vector2DotProduct(vector, surfaceNormal))
-            ));
-}
